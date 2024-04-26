2d12a8856c5cc5c0a02e4abdb9cb6ba184ebe91a
JumanaMurad <jumanamurad1@gmail.com>
2024-04-26 13:26:46 +0200

    feat: Add repositories

---
+++ src/main/java/com/library/librarymanagementsystem/book/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library.librarymanagementsystem.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Integer>
+{
+    List<Book>  findAllByAuthorContaining(String author);
+}
---
+++ src/main/java/com/library/librarymanagementsystem/borrowRecord/BorrowRecordRepository.java
@@ -0,0 +1,7 @@
+package com.library.librarymanagementsystem.borrowRecord;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Integer>
+{
+}
